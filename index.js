d86ff5d6494c46cc5f8065091020af6f472cef87
Justin Williams <justinwilliams@Mac-Studio.attlocal.net>
2023-02-28 01:28:44 -0600

    Responsiveness

---
+++ index.js
@@ -0,0 +1,7 @@
+// NavBar
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navBarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navBarLinks.classList.toggle("active");
+});
